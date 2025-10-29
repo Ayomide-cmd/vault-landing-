a65e7779376e612b94c5db7e563213efa4af4dad
Ayomide-cmd <185790396+Ayomide-cmd@users.noreply.github.com>
2025-10-29 20:58:32 +0100

    Updated mobile navbar behavior

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const body = document.querySelector("body");
 const overlay = document.querySelector(".overlay");
 const hamburger = document.querySelector("#hamburger");
 const close = document.querySelector("#close");
+const overlayClose = document.querySelector(".overlay-close");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.add("hidden");
@@ -15,4 +16,21 @@ close.addEventListener("click", () => {
   close.classList.add("hidden");
   overlay.classList.remove("active"); 
   body.classList.remove("fixbody");
+});
+
+overlayClose.addEventListener("click", () => {
+  hamburger.classList.remove("hidden");
+  close.classList.add("hidden");
+  overlay.classList.remove("active"); 
+  body.classList.remove("fixbody");
+});
+
+const overlayLinks = overlay.querySelectorAll("a");
+overlayLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("hidden");
+    close.classList.add("hidden");
+    overlay.classList.remove("active"); 
+    body.classList.remove("fixbody");
+  });
 });
